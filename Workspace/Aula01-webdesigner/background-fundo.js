ba7d247bda5dc42564df5bc58e21fb989fbf4ac7
Jean Carlo Costa <jeancarlo_fcl@hotmail.com>
2024-05-07 11:48:56 -0300

    att

---
+++ Workspace/Aula01-webdesigner/background-fundo.js
@@ -0,0 +1,11 @@
+function mudarBackground() {
+
+    // Obtendo o elemento do corpo da página
+  
+    var body = document.body;
+  
+    // Definindo o novo background
+  
+    body.style.background = "black"; // Você pode usar qualquer cor válida aqui, como "blue", "#FF0000", "rgba(0, 255, 0, 0.5)", etc.
+  
+  }
---
+++ Workspace/projeto-starbucks/starbucks.js
@@ -0,0 +1,47 @@
+
+const corSite = document.getElementById('cor-botao')
+
+let dark = false;
+
+corSite.addEventListener("click", function(){
+    dark =! dark
+
+    if(dark){
+
+      document.body.style.background = "#000000";
+
+    } else{
+
+      document.body.style.background = "#ffffff";
+    }
+
+})
+
+
+const corAreasSite = document.getElementById('nav-esquerda','nav-direita','destaque','cafe','planta','pessoas','rodape-link','rodape-redes')
+
+let grey = false;
+
+corAreasSite.addEventListener("click", function(){
+    grey != grey
+
+    if(grey){
+      document.body.style.background = "#bdbdbdeb";
+    } else{
+      document.body.style.background = "#ffffff";
+    }
+})
+
+
+//function mudarBackground() {
+
+    // Obtendo o elemento do corpo da página
+  
+    //var body = document.body;
+  
+    // Definindo o novo background
+  
+   // body.style.background = "#000000"; // Você pode usar qualquer cor válida aqui, como "blue", "#FF0000", "rgba(0, 255, 0, 0.5)", etc.
+    
+  
+  //} body.style.background = "#000000";
