f03353c5d8faecb2af0d1423e098026ef009f837
Jean Carlo Costa <jeancarlo_fcl@hotmail.com>
2024-05-20 11:47:18 -0300

    Att

---
+++ Workspace/Api-Rest/src/bancoDeDados.js
@@ -0,0 +1,18 @@
+const bancoDedados = {
+    instrutores: [
+        {
+            id: 1,
+            nome: 'Juscelino',
+            email: 'linooow@gmail.com',
+            status: true
+        },
+        {
+            id: 2,
+            nome: 'Valtemir',
+            email: 'vava@gmail.com',
+            status: true
+        }
+    ]
+}
+
+module.exports = bancoDedados;
